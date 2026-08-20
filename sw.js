a7e322e5060e392f5cf02588aa33408139abdbb9
99Eddie <ibucelos@gmail.com>
2026-08-20 13:41:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='quizapp-v2';
+const CACHE='quizapp-v3';
 const ASSETS=[
-  './index.html','./manifest.json','./icon-192.png','./icon-512.png',
+  './index.html','./manifest.json','./icon-192.png','./icon-512.png','./sample-data.js',
   'https://cdn.jsdelivr.net/npm/yakuhanjp@4.1.0/dist/css/yakuhanjp.min.css',
   'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
 ];
